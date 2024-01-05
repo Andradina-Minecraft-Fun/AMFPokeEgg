21b7a69248f6600756cff373babfd94577242012
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2024-01-05 20:02:58 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AMFPokeEgg
 An pokeegg to catch mobs
+
+# Compile
+
+just clone this repository, and run command
+
+```bash
+mvn compile && mvn package
+```
---
+++ src/main/java/br/com/centralandradina/amfpokeegg/main.java
@@ -0,0 +1,15 @@
+package br.com.centralandradina.amfpokeegg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin
+{
+	@Override
+	public void onLoad() { }
+
+	@Override
+	public void onEnable() { }
+
+	@Override
+	public void onDisable() { }
+}
