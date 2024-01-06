2709150b2b7dc58f71409028e4d1fe9d5c64501d
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2024-01-06 18:50:21 -0300

    first version of translation helper

---
+++ src/main/java/br/com/centralandradina/LanguageUtils.java
@@ -0,0 +1,70 @@
+package br.com.centralandradina;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+
+
+public class LanguageUtils
+{
+
+	protected String language;
+	protected JavaPlugin plugin;
+	private static final Map<String, Map<String, String>> translations = new HashMap<>();
+
+	public LanguageUtils(JavaPlugin plugin)
+	{
+		this.plugin = plugin;
+	}
+
+	public void loadTranslations(String language)
+	{
+		this.language = language;
+		String languageFilePath = this.plugin.getDataFolder() + "/languages/" + language + ".json";
+
+		try {
+			InputStreamReader reader = new InputStreamReader(new FileInputStream(languageFilePath), StandardCharsets.UTF_8);
+			JSONObject json = (JSONObject) new JSONParser().parse(reader);
+			reader.close();
+
+			Map<String, String> mapping = new HashMap<>();
+			for (Object obj : json.keySet()) {
+				try {
+					String key = (String) obj;
+					mapping.put(key, (String) json.get(key));
+				} catch (Exception e) {
+				}
+			}
+			translations.put(language, mapping);
+			
+		}
+		catch (Exception e) {
+			plugin.getLogger().info(e.getMessage());
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	public String getTranslation(String translationKey) {
+		try {
+			Map<String, String> mapping = translations.get(language);
+			if(mapping == null) {
+				return getTranslation(translationKey);
+			}
+			else {
+				return mapping.getOrDefault(translationKey, translationKey);
+			}
+
+		} catch (Exception e) {
+			return translationKey;
+		}
+	}	
+}
